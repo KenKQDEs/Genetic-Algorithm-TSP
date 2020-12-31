0d68fdd3d4b33ab58033202b7c68c3f2363b4c5e
Cosmin M <54987943+KenKQDEs@users.noreply.github.com>
2020-12-31 20:01:15 +0200

    added support for 2d coords

--- InputOutput.h
+++ InputOutput.h
@@ -17,6 +17,30 @@ public:
 		return distante;
 
 	}
+	
+	static std::vector<std::pair<long double, long double>> CitesteCoordonate2D(std::string fileName)
+	{
+	    std::vector<std::pair<long double, long double>> coords;
+	    std::fstream fileStream;
+	    std::string linie;
+	    fileStream.open(fileName, std::fstream::in);
+	    std::getline(fileStream, linie);
+	    int NrLinii = 0;
+	    sscanf_s(linie.c_str(), "%d", &NrLinii);
+	    coords.reserve(NrLinii);
+	
+	    int i = 1;
+	    while (!fileStream.fail() && !fileStream.eof() && i<= NrLinii)
+	    {
+	        double x, y;
+	        std::getline(fileStream, linie);
+	        sscanf_s(linie.c_str(), "%Lf %Lf", &x, &y);
+	        coords.push_back(std::pair<long double, long double>(x, y));
+	        i++;
+	    }
+	    fileStream.close();
+	    return coords;
+	}
 
 };
 
