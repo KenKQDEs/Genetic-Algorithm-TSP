e8b0af643be8b7d9e25ed606cde44c4722c295e5
Dărăbăneanu Cosmin <60667045+Cosmin132@users.noreply.github.com>
2021-01-01 22:52:56 +0200

    Update Crossover.h

--- Crossover.h
+++ Crossover.h
@@ -2,59 +2,41 @@
 #include <iostream>
 #include<vector>
 #include "Generate.h"
+#include "Rand.h"
+#include <map>
+#include <unordered_map>
 class Crossover
 {
- public:
-	//father = [ ff | ss ]
+public:
+  
+    static  std::vector<size_t>SetOff(size_t nr_orase, std::vector<size_t>& father, std::vector<size_t>& mother)
+    {
+        size_t size_pop = 0;
+        std::unordered_map<size_t, int>occurences;
 
-//mother = [ gg | cc]
+       size_t split = RandomHelpers::GetRandomFromRange<int>(1, father.size() - 1);
+       //size_t split = 6;
 
-//offstring A=ff cc
-//offstring B=gg ss
-//luam ff din father si verificam in tot mother daca exista valori de genul si apoi valorile ce raman le punem in mother
-//
-	static std::vector<size_t>SetOffstring(size_t nr_population, std::vector<size_t>parent1, std::vector<size_t>parent2)
-	{
-		std::vector<size_t> offstringA(nr_population, 0);
-		size_t size_pop = 0;
-		size_t split = parent1.size() / 2 + 1;
-	
-		for (int i = 0; i < split; i++)
-		{
-			offstringA[size_pop++] = parent1[i];
-		}
-		bool ok;
-		size_t i;
-		for (i = 0; i < parent2.size(); i++)
+        std::vector<size_t> offstringA(father.begin(), father.begin() + split);
+        for (auto& num : offstringA)
+        {
+            occurences[num]++;
+        }
+        for (auto i = 0; i < mother.size(); i++)
+            if (occurences.find(mother[i]) == occurences.end())
+            {
+                offstringA.push_back(mother[i]);
+                
+            }
+        return offstringA;
+    }
+    static std::pair< std::vector<size_t>, std::vector<size_t>> Crossoverul(size_t nr_orase, std::vector<size_t>& father, std::vector<size_t>& mother)
+    {
 
-		{
-			ok = 0;
-			for (int j = 0; j < offstringA.size(); j++)
-			{
-				if (offstringA[j] == parent2[i])
-				{
-					ok = 1; break;
-				}
+        auto offstringB = SetOff(nr_orase, mother, father);
+        auto offstringA = SetOff(nr_orase, father, mother);
 
-			}
-			if (ok == 0)
-				offstringA[size_pop++] = parent2[i];
-		}
-		return offstringA;
-	}
+        return { offstringA, offstringB };
 
-  static void Crossoverul(size_t nr_population, std::vector<size_t>&offstringA, std::vector<size_t>&offstringB)
-	{
-	  std::vector<size_t> father, mother;
-
-	  size_t size_pop=0;
-	 
-	  Generate p;
-		father = p.Population(nr_population);
-		mother = p.Population(nr_population);
-		offstringA = SetOffstring(nr_population, father, mother);
-		offstringB = SetOffstring(nr_population, mother, father);
-	
-  }
-};
-
+    }
+}; 
